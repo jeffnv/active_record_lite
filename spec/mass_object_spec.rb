bda2dd970882602abc3bcd380621432bb6306dda
CJ Avilla <cjavilla@gmail.com>
2013-10-03 13:00:56 -0700

    update associatable spec

--- spec/mass_object_spec.rb
+++ spec/mass_object_spec.rb
@@ -1,16 +1,15 @@
 require 'active_record_lite'
 
-class MyMassObject < MassObject
-  my_attr_accessible(:x, :y)
-end
-
-# Write your own tests as you need!
-obj = MyMassObject.new(:x => :x_val, :y => :y_val)
-p obj
-
 # Use these if you like.
 describe MassObject do
   subject(:obj) { MyMassObject.new(:x => :x_val, :y => :y_val) }
+
+  before(:all) do
+    class MyMassObject < MassObject
+      my_attr_accessible(:x, :y)
+    end
+  end
+
   it "#my_attr_accessible should add #x and #y" do
     obj.methods.should include(:x)
     obj.methods.should include(:y)
@@ -21,3 +20,7 @@ describe MassObject do
     obj.y.should == :y_val
   end
 end
+
+# Write your own tests as you need!
+obj = MyMassObject.new(:x => :x_val, :y => :y_val)
+p obj
