0b8e67211ebbf1a3c4f0bf920b3eca0b2f072743
CJ Avilla <cjavilla@gmail.com>
2013-10-07 22:35:26 -0700

    more test coverage for searchable_spec

--- spec/searchable_spec.rb
+++ spec/searchable_spec.rb
@@ -16,13 +16,23 @@ describe "searchable" do
     end
   end
 
+  describe "searchable is correctly mixed into SQLObject (extend vs include)" do
+    class Testing123 < SQLObject
+      set_table_name "testing"
+      my_attr_accessible :a, :b, :c
+    end
+    Testing123.methods.should include(:where)
+    t = Testing123.new
+    t.methods.should_not include(:where)
+  end
+
   describe "#where" do
-    it "returns correct cat" do
+    it "returns correct object given a single search term" do
       cat = Cat.where(:name => "Breakfast")[0]
       cat.name.should == "Breakfast"
     end
 
-    it "returns correct human" do
+    it "returns correct object given multiple search terms (Testing AND in WHERE clause.)" do
       human = Human.where(:fname => "Matt", :house_id => 1)[0]
       human.fname.should == "Matt"
     end
