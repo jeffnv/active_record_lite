ce6ccfe90a3b980b6d0d593d0bcea96c715f3a65
CJ Avilla <cjavilla@gmail.com>
2013-10-07 21:41:18 -0700

    remove garbage comments from sql_object_spec

--- spec/sql_object_spec.rb
+++ spec/sql_object_spec.rb
@@ -8,38 +8,27 @@ describe SQLObject do
       File.expand_path(File.join(File.dirname(__FILE__), "cats.db"))
     DBConnection.open(cats_db_file_name)
 
-    class Cat < SQLObject
+    class TestCat < SQLObject
       set_table_name("cats")
       my_attr_accessible(:id, :name, :owner_id)
     end
 
-    class Human < SQLObject
+    class TestHuman < SQLObject
       set_table_name("humans")
       my_attr_accessible(:id, :fname, :lname, :house_id)
     end
-
-    # p Human.find(1)
-    # p Cat.find(1)
-    # p Cat.find(2)
-
-    # p Human.all
-    # p Cat.all
-
-    # c = Cat.new(:name => "Gizmo", :owner_id => 1)
-    # c.save # create
-    # c.save # update
   end
 
   it "#find finds objects by id" do
-    c = Cat.find(1)
+    c = TestCat.find(1)
     expect(c).not_to be_nil
   end
 
   it "#saves saves changes to an object" do
-    h = Human.find(1)
+    h = TestHuman.find(1)
     n = h.fname
     h.fname = SecureRandom.urlsafe_base64(16)
     h.save
-    n.should_not == Human.find(1).fname
+    n.should_not == TestHuman.find(1).fname
   end
 end
